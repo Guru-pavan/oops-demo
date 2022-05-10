a9de89c4dc37818326fb01e38fb47a870dc37813
Guru-pavan <18113025@student.hindustanuniv.ac.in>
2022-05-10 11:00:25 +0530

    IMS version 1.0

---
+++ inventory-management-system/src/test/java/com/wipro/velocity/ims/InventoryManagementSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.wipro.velocity.ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class InventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
